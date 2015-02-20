33a7c4f751d123fd13a108b53267d048cff7407a
Indraneel Purohit <i.m.indraneel@gmail.com>
2015-02-20 15:17:15 -0500

    added footer, rutgers branding, and fixed header for first release

--- shrunk/static/js/shrunk.js
+++ shrunk/static/js/shrunk.js
@@ -3,25 +3,25 @@ var shrunk = shrunk || function() {
 }
 
 var delete_link = function(short_url) {
-	/* Might have trailing whitespace */
-	short_url = short_url.trim();
+    /* Might have trailing whitespace */
+    short_url = short_url.trim();
 
-	/* Create a fake form */
-	var form = document.createElement("form");
+    /* Create a fake form */
+    var form = document.createElement("form");
         form.setAttribute("action", "/delete");
         form.setAttribute("method", "POST");
 
-	var hiddenField = document.createElement("input");
+    var hiddenField = document.createElement("input");
         hiddenField.setAttribute("type", "hidden");
         hiddenField.setAttribute("name", "short_url");
         hiddenField.setAttribute("id", "short_url");
         hiddenField.setAttribute("value", short_url);
 
-	form.appendChild(hiddenField);
+    form.appendChild(hiddenField);
         form.appendChild(document.createElement("input", {type: "submit"}))
 
-	/* Submit the form */
-	document.body.appendChild(form);
-	form.submit();
-	document.body.removeChild(form);
+    /* Submit the form */
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removeChild(form);
 }
