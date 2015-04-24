074bc86a02eb2d36d3293e711dc01288ef7797fb
Kyle Suarez <ksuarz@gmail.com>
2015-04-24 14:02:28 -0400

    Add edit link JavaScript function
    Spoofs a fake form to allow posting to a future route that edits links.

--- shrunk/static/js/shrunk.js
+++ shrunk/static/js/shrunk.js
@@ -26,6 +26,30 @@ var delete_link = function(short_url) {
     document.body.removeChild(form);
 }
 
+var edit_link = function(short_url) {
+    /* Strip trailing whitespace */
+    short_url = short_url.trim();
+
+    /* Create a fake form */
+    var form = document.createElement("form");
+        form.setAttribute("action", "/edit");
+        form.setAttribute("method", "POST");
+
+    var hiddenField = document.createElement("input");
+        hiddenField.setAttribute("type", "hidden");
+        hiddenField.setAttribute("name", "short_url");
+        hiddenField.setAttribute("id", "short_url");
+        hiddenField.setAttribute("value", short_url);
+
+    form.appendChild(hiddenField);
+        form.appendChild(document.createElement("input", {type: "submit"}))
+
+    /* Submit the form */
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removeChild(form);
+}
+
 function toggleLinks(cb, netid) {
     $("article.link-group").each(function() {
         if(cb.checked)
