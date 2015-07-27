08f57bac38876b86585a5b94b1cb4ac713b66378
Jungsoo Park <jungsoopark96@gmail.com>
2015-07-27 16:54:56 -0400

    Fix the backup script.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,14 +5,16 @@ BACKUP_DIR=/var/shrunk
 BACKUP_CONFIG_DIR=$BACKUP_DIR/config
 BACKUP_DUMP_DIR=$BACKUP_DIR/dumps
 
+mkdir -p $BACKUP_CONFIG_DIR $BACKUP_DUMP_DIR
+
 CONFIG=/etc/shrunk/config.py
 
 # how many backups should be kept?
-MAX_BACKUPS=12
+MAX_BACKUPS=4
 
 # rotate dumps
 cd $BACKUP_DUMP_DIR
-for n in $(seq $MAX_BACKUPS -1 1)
+for n in $(seq $MAX_BACKUPS -1 0)
 do
     mv -f dump.$n dump.$(($n+1))
 done
@@ -20,9 +22,9 @@ rm -rf dump.$(($MAX_BACKUPS+1))
 
 mongodump -o dump.0
 
-# rotate logs
+# rotate configs 
 cd $BACKUP_CONFIG_DIR
-for n in $(seq $MAX_BACKUPS -1 1)
+for n in $(seq $MAX_BACKUPS -1 0)
 do
     mv -f config.py.$n config.py.$(($n+1))
 done
