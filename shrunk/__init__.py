67c94bbddb936a6ce6909baff7e2643508c56abf
Alexander Pavel <anp120@nbcs.rutgers.edu>
2018-02-06 11:14:55 -0500

    __init.py__: make it work with honcho

--- shrunk/__init__.py
+++ shrunk/__init__.py
@@ -4,3 +4,15 @@ from shrunk.client import ShrunkClient
 
 version_tuple = (0, 1)
 """The current version of shrunk."""
+import shrunk.appserver
+
+def create_app():
+    return shrunk.appserver.app
+
+
+def wsgi(*args, **kwargs):
+    return create_app()(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    create_app().run()
