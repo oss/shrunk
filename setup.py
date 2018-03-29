2eab7512f6c78219ab3123de2c1495c9ef05770c
Alexander Pavel <alexpavel123@gmail.com>
2018-03-29 11:15:15 -0400

    setup.py: version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ require = [
 
 setup(
     name = "shrunk",
-    version = "0.4.5",
+    version = "0.4.6",
     packages = ["shrunk"],
     install_requires = require,
     package_dir = {"shrunk": "shrunk"},
