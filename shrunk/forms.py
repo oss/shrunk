a0de43f4ae0a0fd2201d530fc778c984e334b80e
ayw19 <ayw19@rutgers.edu>
2018-07-03 17:23:45 -0400

    poweruser implementation that probably works

--- shrunk/forms.py
+++ shrunk/forms.py
@@ -4,7 +4,7 @@
 
 import re
 
-from wtforms import Form, TextField, RadioField, validators, ValidationError
+from wtforms import Form, TextField, RadioField, SelectField, validators, ValidationError
 
 import shrunk.filters
 
@@ -91,8 +91,12 @@ class AddAdminForm(Form):
     :Fields:
       - `netid`: Text field corresponding to a NetID
     """
+
     netid = TextField("NetID", validators=[validators.DataRequired()])
 
+class AddPowerUserForm(Form):
+     #this is actually exactly the same as the previous form, so we might not even need it.
+    netid = TextField("NetID", validators=[validators.DataRequired()])
 
 class BlockLinksForm(Form):
     """A WTForm for blocking unwanted URLs.
