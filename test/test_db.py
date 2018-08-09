28adc55bfb5380e43a9bbb3b7bc3b4c55a10fd2a
mjw271 <mjw271@jla.rutgers.edu>
2018-08-09 18:36:19 -0400

    test blocking logic

--- test/test_db.py
+++ test/test_db.py
@@ -23,7 +23,7 @@ def teardown_module():
     box.stop()
 
 def insert_urls(long_urls, netid):
-    return [client.create_short_url(url, netid=netid) for url in long_urls]
+    return [client.create_short_url(url, netid = netid) for url in long_urls]
 
 def test_urls():
     """Puts and retrieves URLs from the database."""
@@ -35,19 +35,19 @@ def test_urls():
 def test_visit():
     """Tests logic when "visiting" a URL."""
     long_url = "http://www.foobar.net/index"
-    short_url = client.create_short_url(long_url, netid="shrunk_test")
+    short_url = client.create_short_url(long_url, netid = "shrunk_test")
     assert short_url is not None
     
     hits = 4
     for _ in range(0, hits):
         client.visit(short_url, "127.0.0.1")
         
-    assert client.get_num_visits(short_url) == hits
+    assert client.get_num_visits(short_url) is hits
 
 def test_deletion():
     """Tests a deletion from the database."""
     long_url = "foo.com"
-    short_url = client.create_short_url(long_url, netid="shrunk_test")
+    short_url = client.create_short_url(long_url, netid = "shrunk_test")
     assert short_url is not None
     
     client.delete_url(short_url, "shrunk_test")
@@ -60,10 +60,50 @@ def test_count():
     alice_urls = insert_urls(["aa.com", "bb.com"], "alice")
     bob_urls = insert_urls(["cc.com", "dd.com", "ee.com"], "bob")
 
-    assert client.count_links(netid="alice") == 2
-    assert client.count_links(netid="bob") == 3
-    assert client.count_links() == 5
+    assert client.count_links(netid="alice") is 2
+    assert client.count_links(netid="bob") is 3
+    assert client.count_links() is 5
 
 def test_isolated():
     """Sanity check to make sure the db is reset between tests"""
-    assert client.count_links() == 0
+    assert client.count_links() is 0
+
+def test_blocking():
+    """make sure block_link and is_blocked work"""
+    long_urls = ["https://microsoft.com", 
+                 "http://microsoft.com", #should block other protocols
+                 "https://microsoft.com/should-block-paths.aspx",
+                 "https://should-block-subdomains.microsoft.com",
+                 "https://ComL3te-MeSs.mIcroSoft.cOm/of-AllofTh3m/4.aspx"]
+
+    # make sure they start as unblocked
+    urls = insert_urls(long_urls,"bgates")
+    for long_url in long_urls:
+        assert client.is_blocked(long_url) is False
+    
+    # blocking first time should succeed
+    assert client.block_link("https://microsoft.com", blocked_by = "ltorvalds") is not None
+
+    #links the urls we gave should be blocked
+    for long_url in long_urls:
+        assert client.is_blocked(long_url) is True
+    
+    # the urls also should no longer be in the database after being blocked
+    urls_after_block = [client._mongo.shrunk_urls.blocked_urls.find_one(url) for url in urls]
+    for url_after_block in urls_after_block:
+        assert url_after_block is None
+
+    # blocking the link twice should be none to show the block is unsuccesful
+    assert client.block_link("microsoft.com", blocked_by = "ltorvalds") is None
+    
+def test_get_domain():
+    """testing to get the domain from a url"""
+    assert client.get_domain("https://test.com") == "test.com"
+    assert client.get_domain("https://test.com/test.php") == "test.com"
+    assert client.get_domain("https://sub.test.com/test.php") == "test.com"
+    assert client.get_domain("http://sub-sub.anotha.one.TeSt.cOm/test.php") == "test.com"
+    assert client.get_domain("http://sfe9fwlmfwe-f9w0f.fw9e0-i.fJe-FJwef-09.org/shady.cgi") == "fje-fjwef-09.org"
+
+def test_modify():
+    """make sure modifing the url sets the new info properly"""
+    pass
