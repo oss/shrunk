df33ae18fba37cc12cd09c35e05d9e7137df9439
mjw271 <mjw271@jla.rutgers.edu>
2018-08-30 12:41:18 -0400

    factor out decorators, move formatttime to stringutils since it doesnt have many deps, and move from global state to attaching the client to the flask app

--- shrunk/util.py
+++ shrunk/util.py
@@ -4,11 +4,7 @@
 
 import logging
 
-from shrunk.client import ShrunkClient
-from flask import redirect, session
-from functools import wraps
-
-client=None
+#client=None
 def get_db_client(app, g = None):
     """Gets a reference to a ShrunkClient for database operations.
 
@@ -20,16 +16,17 @@ def get_db_client(app, g = None):
       - A reference to a singleton ShrunkClient.
     """
     #TODO depricate using flask's g
-    if not g:
-        global client
-        if not client:
-            client=ShrunkClient(app.config["DB_HOST"], app.config["DB_PORT"])
-        return client
-    else:
-        if not hasattr(g, "client"):
-            g.client = ShrunkClient(app.config["DB_HOST"], app.config["DB_PORT"])
-            
-        return g.client
+    #if not g:
+    #    global client
+    #    if not client:
+    #        client=ShrunkClient(app.config["DB_HOST"], app.config["DB_PORT"])
+    #    return client
+    #else:
+    #    if not hasattr(g, "client"):
+    #        g.client = ShrunkClient(app.config["DB_HOST"], app.config["DB_PORT"])
+    #        
+    #    return g.client
+    return app.get_shrunk()
 
 
 def set_logger(app):
@@ -42,42 +39,3 @@ def set_logger(app):
     handler.setLevel(logging.INFO)
     handler.setFormatter(logging.Formatter(app.config["LOG_FORMAT"]))
     app.logger.addHandler(handler)
-
-
-def formattime(datetime):
-    """Utility function for formatting datetimes.
-
-    This formats datetimes to look like "Nov 19 2015".
-    """
-    return datetime.strftime("%b %d %Y")
-
-#decorator to check if user is logged in
-#it looks like its double wrapped but thats so it can be a decorator that takes in params
-def require_login(app):
-    def decorate(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            client = get_db_client(app)
-            if not 'user' in session:
-                return redirect("/shrunk-login")
-            if client.is_blacklisted(session["user"].get("netid")):
-                return redirect("/unauthorized")
-            return func(*args, **kwargs)
-        return wrapper
-    return decorate
-
-#decorator to check if user is an admin
-#it looks like its double wrapped but thats so it can be a decorator that takes in params
-def require_admin(app):
-    def decorate(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            client = get_db_client(app)
-            netid = session["user"].get("netid")
-            if client.is_blacklisted(netid):
-                return redirect("/unauthorized")
-            if not client.is_admin(netid):
-                return redirect("/")
-            return func(*args, **kwargs)
-        return wrapper
-    return decorate
