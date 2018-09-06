d869ea9044f6cbaacc1022eb6314a015e7ff7ee0
mjw271 <mjw271@jla.rutgers.edu>
2018-09-06 17:01:33 -0400

    fix shib login expression

--- shrunk/appserver.py
+++ shrunk/appserver.py
@@ -53,9 +53,9 @@ add_roles_routes(app)
 @ext.login_handler
 def login(user_info):
     client = app.get_shrunk()
-    if user_info.get("employeeType") not in app.config["VALID_EMPLOYEE_TYPES"] \
-    and user_info.get("netid") not in app.config["USER_WHITELIST"] \
-    and client.is_blacklisted(user_info.get("netid")):
+    if (user_info.get("employeeType") not in app.config["VALID_EMPLOYEE_TYPES"] \
+    and user_info.get("netid") not in app.config["USER_WHITELIST"]) \
+    or client.is_blacklisted(user_info.get("netid")):
         return redirect("/unauthorized")
     session["user"] = user_info
     return redirect("/")   
