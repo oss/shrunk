b58267f77a6c6f5c5ee44589672277fbe955e604
mjw271 <mjw271@jla.rutgers.edu>
2018-09-10 17:32:56 -0400

    remove dependancy for validators

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ require = [
     'Werkzeug>=0.9.6',
     'itsdangerous>=0.24',
     'pyasn1>=0.1.7',
-    'pymongo>=2.7.2',
+    'pymongo>=3.6.1',
 ]
 
 setup(
     name = "shrunk",
-    version = "0.4.10",
+    version = "0.5.3",
     packages = ["shrunk"],
     install_requires = require,
     package_dir = {"shrunk": "shrunk"},
