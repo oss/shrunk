fd4b0c8525b97c6f474bb69df97287868125c485
Mickey_J <mjw271@jla.rutgers.edu>
2019-02-20 16:01:37 -0500

    fix syntax error

--- scripts/base.sh
+++ scripts/base.sh
@@ -8,7 +8,7 @@ fi
 #get the key vai post from stdin                                                                                                     
 KEY=$(cat <&0)
 SECRET=$(cat /var/www/checksecret)
-if [[ $KEY != SECRET ]]; then
+if [[ $KEY != $SECRET ]]; then
     echo -en "Status: 401 must send key via post\n\n"
     exit 0
 fi
