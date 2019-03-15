fe14b8221b750af0dd07bb3a833e4d4be311a83c
Mickey_J <mjw271@jla.rutgers.edu>
2019-03-15 16:10:08 -0400

    move shorturl handler into shrunkFlaskMini

--- shrunk/appserver.py
+++ shrunk/appserver.py
@@ -105,40 +105,7 @@ def unauthorized():
     return make_response(render_template('unauthorized.html'))
 
 ### Views ###
-try:
-    if app.config["DUAL_SERVER"]:
-        @app.route("/<short_url>")
-        def redirect_link(short_url):
-            """Redirects to the short URL's true destination.
-
-            This looks up the short URL's destination in the database and performs a
-            redirect, logging some information at the same time. If no such link
-            exists, a not found page is shown.
-
-            :Parameters:
-              - `short_url`: A string containing a shrunk-ified URL.
-            """
-            client = app.get_shrunk()
-            app.logger.info("{} requests {}".format(request.remote_addr, short_url))
-
-            # Perform a lookup and redirect
-            long_url = client.get_long_url(short_url)
-            if long_url is None:
-                return render_template("link-404.html", short_url=short_url)
-            else:
-                client.visit(short_url, request.remote_addr,
-                             request.headers.get('User-Agent'),
-                             request.headers.get('Referer'))
-                # Check if a protocol exists
-                if "://" in long_url:
-                    return redirect(long_url)
-                else:
-                    return redirect("http://{}".format(long_url))
-except KeyError:
-    # No setting in the config file
-    pass
-
-
+# route /<short url> handle by shrunkFlaskMini
 
 @app.route("/")
 @app.require_login
--- shrunk/linkserver.py
+++ shrunk/linkserver.py
@@ -11,36 +11,7 @@ app = ShrunkFlaskMini(__name__)
 
 
 ### Views ###
-@app.route("/<short_url>")
-def redirect_link(short_url):
-    """Redirects to the short URL's true destination.
-
-    This looks up the short URL's destination in the database and performs a
-    redirect, logging some information at the same time. If no such link exists,
-    a not found page is shown.
-
-    :Parameters:
-      - `short_url`: A string containing a shrunk-ified URL.
-    """
-    client = app.get_shrunk()
-    app.logger.info("{} requests {}".format(request.remote_addr, short_url))
-
-    # Perform a lookup and redirect
-    long_url = client.get_long_url(short_url)
-    if long_url is None:
-        return render_template("link-404.html", short_url=short_url)
-    else:
-        client.visit(short_url, request.remote_addr,
-                     request.headers.get('User-Agent'),
-                     request.headers.get('Referer'))
-
-        # Check if a protocol exists
-        if "://" in long_url:
-            return redirect(long_url)
-        else:
-            return redirect("http://{}".format(long_url))
-
-
+# route /<short url> handle by shrunkFlaskMini
 @app.route("/")
 def render_index():
     """Renders the homepage.
