065a2658962deadcde7236e00db1cc9924b378da
Mickey_J <mjw271@jla.rutgers.edu>
2019-05-03 14:46:52 -0400

    bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ require = [
 
 setup(
     name = "shrunk",
-    version = "0.6.3",
+    version = "0.6.4",
     packages = ["shrunk"],
     install_requires = require,
     package_dir = {"shrunk": "shrunk"},
