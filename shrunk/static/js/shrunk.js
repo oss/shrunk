09abf1ae949568afca87f3a4eb5d151210407502
Peter Bandziukas <peb60@jla.rutgers.edu>
2019-06-11 09:50:58 -0400

    make buttons on index work

--- shrunk/static/js/shrunk.js
+++ shrunk/static/js/shrunk.js
@@ -3,33 +3,6 @@ var shrunk = shrunk || function() {
 }
 
 /**
- * Fake a form to delete a link.
- */
-var delete_link = function(short_url) {
-    /* Might have trailing whitespace */
-    short_url = short_url.trim();
-
-    /* Create a fake form */
-    var form = document.createElement("form");
-        form.setAttribute("action", "/delete");
-        form.setAttribute("method", "POST");
-
-    var hiddenField = document.createElement("input");
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("name", "short_url");
-        hiddenField.setAttribute("id", "short_url");
-        hiddenField.setAttribute("value", short_url);
-
-    form.appendChild(hiddenField);
-        form.appendChild(document.createElement("input", {type: "submit"}))
-
-    /* Submit the form */
-    document.body.appendChild(form);
-    form.submit();
-    document.body.removeChild(form);
-}
-
-/**
  * Change the order in which links are sorted on the front page.
  *
  * Forces a redirect to the index.
