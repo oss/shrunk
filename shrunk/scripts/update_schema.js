c2d2b6844ebf7a307e72912b3d62b47cbecb1f18
peb60 <peb60@jla.rutgers.edu>
2019-08-13 14:09:14 -0400

    consolidate db upgrade scripts

---
+++ shrunk/scripts/update_schema.js
@@ -0,0 +1,39 @@
+admin = new Mongo().getDB('admin');
+shrunk = new Mongo().getDB('shrunk');
+
+// put all the collections under the "shrunk" database, drop the old collections
+print('[1/3] Renaming collections...');
+admin.getMongo().getDB('shrunk').dropDatabase();
+admin.adminCommand({ renameCollection: 'shrunk_roles.grants', to: 'shrunk.grants' });
+admin.adminCommand({ renameCollection: 'shrunk_urls.urls', to: 'shrunk.urls' });
+admin.adminCommand({ renameCollection: 'shrunk_visits.visits', to: 'shrunk.visits' });
+admin.adminCommand({ renameCollection: 'shrunk_visits.visitors', to: 'shrunk.visitors' });
+admin.getMongo().getDB('shrunk_roles').dropDatabase();
+admin.getMongo().getDB('shrunk_urls').dropDatabase();
+admin.getMongo().getDB('shrunk_visits').dropDatabase();
+admin.getMongo().getDB('shrunk_users').dropDatabase();
+
+// remove leading whitespace from titles
+print('[2/3] Removing leading whitespace...');
+shrunk.urls.find({'title': {'$regex': '^ .*'}}).forEach(function (doc) {
+    shrunk.urls.update({'_id': doc._id}, {'$set': {'title': doc.title.trim()}});
+});
+
+// normalize the urls and visits collections
+print('[3/3] Normalizing db...');
+let urls = shrunk.urls.aggregate([
+    {'$addFields': {'short_url': '$_id'}},
+    {'$project': {'_id': 0}}
+]).toArray().map(function (doc) {
+    doc.visits = NumberInt(doc.visits);
+    return doc;
+});
+shrunk.urls.remove({});
+shrunk.urls.insert(urls);
+shrunk.urls.find({}).forEach(function (doc) {
+    shrunk.visits.updateMany({'short_url': doc.short_url},
+			     {'$set': {'link_id': doc._id},
+			      '$unset': {'short_url': ''}});
+});
+
+print('Done!');
--- shrunk/scripts/whitespace.js
+++
@@ -1,5 +0,0 @@
-// This script assumes the new schema (i.e. all collections in the 'shrunk' db)
-db = new Mongo().getDB('shrunk');
-db.urls.find({'title': {'$regex': '^ .*'}}).forEach(function (doc) {
-    db.urls.update({'_id': doc._id}, {'$set': {'title': doc.title.trim()}});
-});
