bcfa0e3fb750c08ea350f070acbc0597600cc42d
Mickey_J <mjw271@jla.rutgers.edu>
2019-09-06 17:42:17 -0400

    init refactor for schem change

--- shrunk/shrunk/client.py
+++ shrunk/shrunk/client.py
@@ -158,9 +158,8 @@ class ShrunkClient:
         self.db.visits.create_index([('link_id', pymongo.ASCENDING)])
         self.db.visitors.create_index([('ip', pymongo.ASCENDING)], unique=True)
         self.db.organizations.create_index([('name', pymongo.ASCENDING)], unique=True)
-        self.db.organization_members.create_index([('name', pymongo.ASCENDING),
-                                                   ('netid', pymongo.ASCENDING)],
-                                                  unique=True)
+        self.db.organizations.create_index([('members.name', pymongo.ASCENDING),
+                                            ('members.netid', pymongo.ASCENDING)])
 
     def drop_database(self):
         if self._TESTING:
@@ -168,7 +167,7 @@ class ShrunkClient:
 
     def reset_database(self):
         if self._TESTING:
-            for col in ['grants', 'organization_members', 'organizations',
+            for col in ['grants', 'organizations',
                         'urls', 'visitors', 'visits']:
                 self.db[col].delete_many({})
 
@@ -667,79 +666,89 @@ class ShrunkClient:
     def create_organization(self, name):
         col = self.db.organizations
         rec_query = {'name': name}
-        rec_insert = {'name': name, 'timeCreated': datetime.datetime.now()}
+        rec_insert = {'name': name, 'timeCreated': datetime.datetime.now(), 'members': []}
         res = col.find_one_and_update(rec_query, {'$setOnInsert': rec_insert}, upsert=True,
                                       return_document=ReturnDocument.BEFORE)
         # return false if organization already existed, otherwise true
         return res is None
 
     def delete_organization(self, name):
-        with self._mongo.start_session() as s:
-            s.start_transaction()
-            self.db.organization_members.delete_many({'name': name})
-            self.db.organizations.delete_one({'name': name})
-            s.commit_transaction()
+        self.db.organizations.delete_one({'name': name})
 
     def get_organization_info(self, name):
         col = self.db.organizations
         return col.find_one({'name': name})
 
     def is_organization_member(self, name, netid):
-        col = self.db.organization_members
-        res = col.find_one({'name': name, 'netid': netid})
+        col = self.db.organizations
+        res = col.find_one({'name': name, 'members.netid': netid})
         return bool(res)
 
     def is_organization_admin(self, name, netid):
-        col = self.db.organization_members
-        res = col.find_one({'name': name, 'netid': netid})
-        return res['is_admin'] if res else False
+        col = self.db.organizations
+        res = col.aggregate({'name': name, 'members': {'$elemMatch': 
+                                                       {'netid': netid, 'is_admin': True}}})
+        return bool(res)
 
     def add_organization_member(self, name, netid, is_admin=False):
-        col = self.db.organization_members
-        rec = {'name': name, 'netid': netid}
+        col = self.db.organizations
+        rec = {'name': name}
         rec_insert = {'name': name,
                       'is_admin': is_admin,
                       'netid': netid,
                       'timeCreated': datetime.datetime.now()}
-        res = col.find_one_and_update(rec, {'$setOnInsert': rec_insert},
-                                      upsert=True, return_document=ReturnDocument.BEFORE)
+        res = col.update_one(rec, {'members': {'$addToSet': rec_insert}})
         return res is None
 
     def add_organization_admin(self, name, netid):
-        self.add_organization_member(name, netid, is_admin=True)
-        return True
+        if is_organization_member(name, netid):
+            col = self.db.organizations
+            match = {'name': name, 'members.netid': netid}
+            update = {"members.$.is_admin": true}
+            res = col.update_one(match, update)
+            return bool(res)
+        else:
+            return self.add_organization_member(name, netid, is_admin=True)
+            
 
     def remove_organization_member(self, name, netid):
-        col = self.db.organization_members
-        col.delete_one({'name': name, 'netid': netid})
+        col = self.db.organizations
+        col.update_one({'name': name}, {'$pull': {'members': {'netid': netid}}})
 
     def remove_organization_admin(self, name, netid):
-        col = self.db.organization_members
-        col.update_one({'name': name, 'netid': netid}, {'$set': {'is_admin': False}})
+        col = self.db.organizations
+        col.update_one({'name': name, 'members.netid': netid},
+                       {'$set': {'members.$.is_admin': False}})
 
+    agg_members = [{'$match': {'name': name}},
+                   {'$unwind': 'members'}]
+    agg_count = [{'$count': 'count'}]
+    agg_admins = [{'$match': {'is_admin': True}}]
+    
     def count_organization_members(self, name):
-        col = self.db.organization_members
-        return col.count_documents({'name': name})
+        col = self.db.organizations
+        return col.aggregate(agg_members + agg_count)
 
     def get_organization_members(self, name):
-        col = self.db.organization_members
-        return col.find({'name': name})
+        col = self.db.organizations
+        return col.aggregate(agg_members)
 
     def count_organization_admins(self, name):
-        col = self.db.organization_members
-        return col.count_documents({'name': name, 'is_admin': True})
+        col = self.db.organizations
+        return col.aggregate(agg_members + agg_admins + agg_count)
 
     def get_organization_admins(self, name):
-        col = self.db.organization_members
-        return col.find({'name': name, 'is_admin': True})
+        col = self.db.organizations
+        return col.aggregate(agg_members + admins)
 
     def get_member_organizations(self, netid):
-        col = self.db.organization_members
-        return col.find({'netid': netid})
+        col = self.db.organizations
+        return col.find({'members.netid': netid}, projection={'members': False})
 
     def get_admin_organizations(self, netid):
-        col = self.db.organization_members
-        return col.find({'netid': netid, 'is_admin': True})
+        col = self.db.organizations
+        return col.find({'members': {'$elemMatch': {'netid': netid, 'is_admin': True}}}
+                        projection={'members': False})
 
     def may_manage_organization(self, name, netid):
         if not self.get_organization_info(name):
