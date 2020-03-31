72db6f3040c63d67d57827ad95e565293e497f82
Andy Lam <abl101@jla.rutgers.edu>
2020-03-31 14:01:16 -0400

    modified changes to __init__.py whitespace problem

--- shrunk/client/__init__.py
+++ shrunk/client/__init__.py
@@ -169,8 +169,8 @@ class ShrunkClient(SearchClient, GeoipClient, OrgsClient, TrackingClient):
             "timeCreated": datetime.datetime.now(),
             "visits": 0,
             "deleted": False
-	    "creator_ip": creator_ip
-        }
+            "creator_ip": creator_ip
+	}
         if netid is not None:
             document["netid"] = netid
         if title is not None:
@@ -195,7 +195,7 @@ class ShrunkClient(SearchClient, GeoipClient, OrgsClient, TrackingClient):
                         url = ShrunkClient._generate_unique_key()
 
                     document["short_url"] = url
-		    response = self.db.urls.insert_one(document)
+                    response = self.db.urls.insert_one(document)
                 except pymongo.errors.DuplicateKeyError:
                     continue
 
