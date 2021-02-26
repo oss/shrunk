9d451a27e1c1e9973f61c704e6d52fe5a8f34019
Sam Ping <samuelping42@gmail.com>
2021-02-26 15:42:51 -0500

    Organize page components into their own directories

--- frontend/src/Shrunk.tsx
+++ frontend/src/Shrunk.tsx
@@ -3,50 +3,57 @@
  * @packageDocumentation
  */
 
-import React from 'react';
-import { HashRouter, Switch, Route, Redirect, Link, NavLink } from 'react-router-dom';
-import { createBrowserHistory, Location } from 'history';
-import { Layout, Menu } from 'antd';
+import React from "react";
+import {
+  HashRouter,
+  Switch,
+  Route,
+  Redirect,
+  Link,
+  NavLink,
+} from "react-router-dom";
+import { createBrowserHistory, Location } from "history";
+import { Layout, Menu } from "antd";
 
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 
-import { Dashboard } from './Dashboard';
-import { Stats } from './Stats';
-import { Admin } from './Admin';
-import { AdminStats } from './AdminStats';
-import { Role } from './Role';
-import { Orgs } from './Orgs';
-import { ManageOrg } from './ManageOrg';
-import { OrgStats } from './OrgStats';
-import { Faq } from './Faq';
-import './antd_themed.less';
-import './Shrunk.less';
-import { PendingAlerts } from './alerts/PendingAlerts';
-import { PendingRequests } from './PendingRequests';
+import { Dashboard } from "./pages/Dashboard";
+import { Stats } from "./Stats";
+import { Admin } from "./pages/Admin";
+import { AdminStats } from "./admin/AdminStats";
+import { Role } from "./admin/Role";
+import { Orgs } from "./Orgs";
+import { ManageOrg } from "./ManageOrg";
+import { OrgStats } from "./OrgStats";
+import { Faq } from "./pages/Faq";
+import "./antd_themed.less";
+import "./Shrunk.less";
+import { PendingAlerts } from "./alerts/PendingAlerts";
+import { PendingRequests } from "./PendingRequests";
 
 /**
  * Properties of the [[Shrunk]] component.
  * @interface
  */
 export interface Props {
-    /**
-     * NetID of the user.
-     * @property
-     */
-    netid: string;
+  /**
+   * NetID of the user.
+   * @property
+   */
+  netid: string;
 
-    /**
-     * Width of the siders in the layout.
-     * @property
-     */
-    siderWidth: number;
+  /**
+   * Width of the siders in the layout.
+   * @property
+   */
+  siderWidth: number;
 
-    /**
-     * A set of the user's privileges.
-     * @property
-     */
-    userPrivileges: Set<string>;
+  /**
+   * A set of the user's privileges.
+   * @property
+   */
+  userPrivileges: Set<string>;
 }
 
 /**
@@ -54,32 +61,32 @@ export interface Props {
  * @interface
  */
 interface State {
-    /**
-     * Whether the "Admin" tab should be shown in the top navbar. This is
-     * determined based on the user's privileges.
-     * @property
-     */
-    showAdminTab: boolean;
+  /**
+   * Whether the "Admin" tab should be shown in the top navbar. This is
+   * determined based on the user's privileges.
+   * @property
+   */
+  showAdminTab: boolean;
 
-    /**
-     * Whether the "Whitelist" tab should be shown in the top navbar. This is
-     * determined based on the user's privileges.
-     * @property
-     */
-    showWhitelistTab: boolean;
+  /**
+   * Whether the "Whitelist" tab should be shown in the top navbar. This is
+   * determined based on the user's privileges.
+   * @property
+   */
+  showWhitelistTab: boolean;
 
-    /**
-     * This determines which tabs in the navbar are highlighted. This is determined
-     * based on the active route.
-     * @property
-     */
-    selectedKeys: Array<string>;
+  /**
+   * This determines which tabs in the navbar are highlighted. This is determined
+   * based on the active route.
+   * @property
+   */
+  selectedKeys: Array<string>;
 
-    /**
-     * The alerts which need to be displayed to the user, in increasing order.
-     * @property
-     */
-    pendingAlerts: Array<string>;
+  /**
+   * The alerts which need to be displayed to the user, in increasing order.
+   * @property
+   */
+  pendingAlerts: Array<string>;
 }
 
 /**
@@ -89,169 +96,229 @@ interface State {
  * @class
  */
 export class Shrunk extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        const showAdminTab = this.props.userPrivileges.has('admin');
-        const showWhitelistTab = !showAdminTab && this.props.userPrivileges.has('facstaff');
-        this.state = {
-            showAdminTab,
-            showWhitelistTab,
-            selectedKeys: ['dashboard'],
-            pendingAlerts: [],
-        };
+  constructor(props: Props) {
+    super(props);
+    const showAdminTab = this.props.userPrivileges.has("admin");
+    const showWhitelistTab =
+      !showAdminTab && this.props.userPrivileges.has("facstaff");
+    this.state = {
+      showAdminTab,
+      showWhitelistTab,
+      selectedKeys: ["dashboard"],
+      pendingAlerts: [],
+    };
+  }
+
+  async componentDidMount(): Promise<void> {
+    await this.updatePendingAlerts();
+    const history = createBrowserHistory();
+    this.setSelectedKeysFromLocation(history.location);
+    history.listen(({ location }) =>
+      this.setSelectedKeysFromLocation(location)
+    );
+  }
+
+  /**
+   * Fetches list of pending alerts from backend and updates state.
+   * @method
+   */
+  updatePendingAlerts = async (): Promise<void> => {
+    await fetch(`/api/v1/alert/${this.props.netid}`)
+      .then((resp) => resp.json())
+      .then((json) =>
+        this.setState({ pendingAlerts: json.pending_alerts as Array<string> })
+      );
+  };
+
+  /**
+   * Sets the active tabs in the navbar based on the current view.
+   * @method
+   * @param location The location given by the history.listen event
+   */
+  setSelectedKeysFromLocation = (location: Location): void => {
+    const route = location.hash;
+    let key: string | null = null;
+    if (route.startsWith("#/dash") || route.startsWith("#/stats")) {
+      key = "dash";
+    } else if (route.startsWith("#/orgs")) {
+      key = "orgs";
+    } else if (route.startsWith("#/admin")) {
+      key = "admin";
+    } else if (route.startsWith("#/roles")) {
+      if (this.state.showWhitelistTab) {
+        key = "whitelist";
+      } else {
+        key = "admin";
+      }
+    } else if (route.startsWith("#/faq")) {
+      key = "faq";
     }
 
-    async componentDidMount(): Promise<void> {
-        await this.updatePendingAlerts();
-        const history = createBrowserHistory();
-        this.setSelectedKeysFromLocation(history.location);
-        history.listen(({ location }) => this.setSelectedKeysFromLocation(location));
+    if (key === null) {
+      throw new Error(`unknown route ${route}`);
     }
 
-    /**
-     * Fetches list of pending alerts from backend and updates state.
-     * @method
-     */
-    updatePendingAlerts = async (): Promise<void> => {
-        await fetch(`/api/v1/alert/${this.props.netid}`)
-            .then(resp => resp.json())
-            .then(json => this.setState({ pendingAlerts: json.pending_alerts as Array<string> }));
-    }
+    this.setState({ selectedKeys: [key] });
+  };
 
-    /**
-     * Sets the active tabs in the navbar based on the current view.
-     * @method
-     * @param location The location given by the history.listen event
-     */
-    setSelectedKeysFromLocation = (location: Location): void => {
-        const route = location.hash;
-        let key: string | null = null;
-        if (route.startsWith('#/dash') || route.startsWith('#/stats')) {
-            key = 'dash';
-        } else if (route.startsWith('#/orgs')) {
-            key = 'orgs';
-        } else if (route.startsWith('#/admin')) {
-            key = 'admin';
-        } else if (route.startsWith('#/roles')) {
-            if (this.state.showWhitelistTab) {
-                key = 'whitelist';
-            } else {
-                key = 'admin';
-            }
-        } else if (route.startsWith('#/faq')) {
-            key = 'faq';
-        }
+  render(): React.ReactNode {
+    return (
+      <HashRouter>
+        <Layout>
+          <Header className="header">
+            <div className="logo">
+              <Link to="/dash">
+                <img
+                  alt="Rutgers"
+                  src="/app/static/img/RU_LOGOTYPE_REVWHITE.png"
+                  width="175px"
+                />
+              </Link>
+            </div>
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              selectedKeys={this.state.selectedKeys}
+            >
+              <Menu.Item key="dash">
+                <NavLink to="/dash" className="nav-text">
+                  Dashboard
+                </NavLink>
+              </Menu.Item>
+              {!this.state.showAdminTab ? (
+                <></>
+              ) : (
+                <Menu.Item key="admin">
+                  <NavLink to="/admin" className="nav-text">
+                    Admin
+                  </NavLink>
+                </Menu.Item>
+              )}
+              {!this.state.showWhitelistTab ? (
+                <></>
+              ) : (
+                <Menu.Item key="whitelist">
+                  <NavLink to="/roles/whitelisted" className="nav-text">
+                    Whitelist
+                  </NavLink>
+                </Menu.Item>
+              )}
+              <Menu.Item key="orgs">
+                <NavLink to="/orgs" className="nav-text">
+                  Organizations
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="faq">
+                <NavLink to="/faq" className="nav-text">
+                  FAQ
+                </NavLink>
+              </Menu.Item>
+              <SubMenu
+                key="user"
+                title={this.props.netid}
+                style={{ float: "right" }}
+              >
+                <Menu.Item key="logout">
+                  <a href="/app/logout">Logout</a>
+                </Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Header>
+          <Layout>
+            <Sider
+              width={this.props.siderWidth}
+              style={{ background: "white" }}
+            />
+            <Content
+              className="main-content"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              {this.state.pendingAlerts === [] ? (
+                <></>
+              ) : (
+                <PendingAlerts
+                  netid={this.props.netid}
+                  pendingAlerts={this.state.pendingAlerts}
+                />
+              )}
+              <PendingRequests />
+              <Switch>
+                <Route exact path="/">
+                  <Redirect to="/dash" />
+                </Route>
 
-        if (key === null) {
-            throw new Error(`unknown route ${route}`);
-        }
+                <Route exact path="/dash">
+                  <Dashboard userPrivileges={this.props.userPrivileges} />
+                </Route>
 
-        this.setState({ selectedKeys: [key] });
-    }
+                <Route
+                  exact
+                  path="/stats/:id"
+                  render={(props) => <Stats id={props.match.params.id} />}
+                />
 
-    render(): React.ReactNode {
-        return (
-            <HashRouter>
-                <Layout>
-                    <Header className='header'>
-                        <div className='logo'>
-                            <Link to='/dash'>
-                                <img alt='Rutgers' src='/app/static/img/RU_LOGOTYPE_REVWHITE.png' width='175px' />
-                            </Link>
-                        </div>
-                        <Menu theme='dark' mode='horizontal' selectedKeys={this.state.selectedKeys}>
-                            <Menu.Item key='dash'>
-                                <NavLink to='/dash' className='nav-text'>Dashboard</NavLink>
-                            </Menu.Item>
-                            {!this.state.showAdminTab ? <></> :
-                                <Menu.Item key='admin'>
-                                    <NavLink to='/admin' className='nav-text'>Admin</NavLink>
-                                </Menu.Item>}
-                            {!this.state.showWhitelistTab ? <></> :
-                                <Menu.Item key='whitelist'>
-                                    <NavLink to='/roles/whitelisted' className='nav-text'>Whitelist</NavLink>
-                                </Menu.Item>}
-                            <Menu.Item key='orgs'>
-                                <NavLink to='/orgs' className='nav-text'>Organizations</NavLink>
-                            </Menu.Item>
-                            <Menu.Item key='faq'>
-                                <NavLink to='/faq' className='nav-text'>FAQ</NavLink>
-                            </Menu.Item>
-                            <SubMenu key='user' title={this.props.netid} style={{ float: 'right' }}>
-                                <Menu.Item key='logout'>
-                                    <a href='/app/logout'>Logout</a>
-                                </Menu.Item>
-                            </SubMenu>
-                        </Menu>
-                    </Header>
-                    <Layout>
-                        <Sider width={this.props.siderWidth} style={{ background: 'white' }} />
-                        <Content
-                            className='main-content'
-                            style={{
-                                padding: 24,
-                                margin: 0,
-                                minHeight: 280,
-                            }}>
-                            {this.state.pendingAlerts === [] ? <></> : <PendingAlerts netid={this.props.netid} pendingAlerts={this.state.pendingAlerts} />}
-                            <PendingRequests />
-                            <Switch>
-                                <Route exact path='/'>
-                                    <Redirect to='/dash' />
-                                </Route>
+                <Route exact path="/orgs">
+                  <Orgs userPrivileges={this.props.userPrivileges} />
+                </Route>
 
-                                <Route exact path='/dash'>
-                                    <Dashboard userPrivileges={this.props.userPrivileges} />
-                                </Route>
+                <Route exact path="/orgs/:id/manage">
+                  <ManageOrg
+                    userNetid={this.props.netid}
+                    userPrivileges={this.props.userPrivileges}
+                  />
+                </Route>
 
-                                <Route
-                                    exact
-                                    path='/stats/:id'
-                                    render={(props) => <Stats id={props.match.params.id} />} />
+                <Route
+                  exact
+                  path="/orgs/:id/stats"
+                  render={(props) => <OrgStats id={props.match.params.id} />}
+                />
 
-                                <Route exact path='/orgs'>
-                                    <Orgs userPrivileges={this.props.userPrivileges} />
-                                </Route>
+                <Route exact path="/faq">
+                  <Faq />
+                </Route>
 
-                                <Route
-                                    exact
-                                    path='/orgs/:id/manage'>
-                                    <ManageOrg userNetid={this.props.netid} userPrivileges={this.props.userPrivileges} />
-                                </Route>
+                <Route
+                  exact
+                  path="/roles/:name"
+                  render={(props) => (
+                    <Role
+                      userPrivileges={this.props.userPrivileges}
+                      name={props.match.params.name}
+                    />
+                  )}
+                />
 
-                                <Route
-                                    exact
-                                    path='/orgs/:id/stats'
-                                    render={(props) => <OrgStats id={props.match.params.id} />} />
-
-                                <Route exact path='/faq'>
-                                    <Faq />
-                                </Route>
-
-                                <Route
-                                    exact
-                                    path='/roles/:name'
-                                    render={(props) => <Role userPrivileges={this.props.userPrivileges} name={props.match.params.name} />} />
-
-                                {!this.state.showAdminTab ? <></> :
-                                    <>
-                                        <Route exact path='/admin'>
-                                            <Admin />
-                                        </Route>
-                                        <Route exact path='/admin/stats'>
-                                            <AdminStats />
-                                        </Route>
-                                    </>}
-                            </Switch>
-                        </Content>
-                        <Sider width={this.props.siderWidth} style={{ background: 'white' }} />
-                    </Layout>
-                    <Footer style={{ textAlign: 'center', color: '#f0f0f0' }}>
-                        &copy;{new Date().getFullYear()}&mdash;Rutgers, The State University of New Jersey&mdash;Questions? Bugs? Contact us:&nbsp;
-                            <a href='mailto:oss@oss.rutgers.edu'>oss@oss.rutgers.edu</a>
-                    </Footer>
-                </Layout>
-            </HashRouter >
-        );
-    }
+                {!this.state.showAdminTab ? (
+                  <></>
+                ) : (
+                  <>
+                    <Route exact path="/admin">
+                      <Admin />
+                    </Route>
+                    <Route exact path="/admin/stats">
+                      <AdminStats />
+                    </Route>
+                  </>
+                )}
+              </Switch>
+            </Content>
+            <Sider
+              width={this.props.siderWidth}
+              style={{ background: "white" }}
+            />
+          </Layout>
+          <Footer style={{ textAlign: "center", color: "#f0f0f0" }}>
+            &copy;{new Date().getFullYear()}&mdash;Rutgers, The State University
+            of New Jersey&mdash;Questions? Bugs? Contact us:&nbsp;
+            <a href="mailto:oss@oss.rutgers.edu">oss@oss.rutgers.edu</a>
+          </Footer>
+        </Layout>
+      </HashRouter>
+    );
+  }
 }
---
+++ frontend/src/admin/AdminStats.tsx
@@ -0,0 +1,275 @@
+/**
+ * Implements the [[AdminStats]] component
+ * @packageDocumentation
+ */
+
+import React from "react";
+
+import { Row, Col, Spin, DatePicker, Form, Button } from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+import { MENU_ITEMS } from "../StatsCommon";
+
+import "../Base.less";
+const { RangePicker } = DatePicker;
+
+/**
+ * Results of an admin stats query to the backend
+ * @interface
+ */
+interface AdminStatsData {
+  /**
+   * Total number of links created during the specified time period
+   * @property
+   */
+  links: number;
+
+  /**
+   * Total number of visits occurring during the specified time period
+   * @property
+   */
+  visits: number;
+
+  /**
+   * Total number of distinct NetIDs creating links during the specified time period
+   */
+  users: number;
+}
+
+/**
+ * Statistics about visits to a single Flask endpoint
+ * @interface
+ */
+interface EndpointDatum {
+  /**
+   * Name of the Flask endpoint
+   * @property
+   */
+  endpoint: string;
+
+  /**
+   * Total number of visits
+   * @property
+   */
+  total_visits: number;
+
+  /**
+   * Total number of unique visits by NetID
+   * @property
+   */
+  unique_visits: number;
+}
+
+/**
+ * The [[EndpointChart]] component displays a bar graph of visits to Flask
+ * endpoints
+ * @function
+ * @param props Props
+ */
+const EndpointChart: React.FC<{ endpointData: EndpointDatum[] | null }> = (
+  props
+) => {
+  if (props.endpointData === null) {
+    return <Spin size="large" />;
+  }
+
+  const options = {
+    chart: { type: "bar" },
+    title: { text: "Endpoint visits" },
+    exporting: { buttons: { contextButton: { menuItems: MENU_ITEMS } } },
+    xAxis: {
+      categories: props.endpointData.map((datum) => datum.endpoint),
+      title: { text: "Endpoint" },
+    },
+    yAxis: {
+      min: 0,
+      title: { text: "Visits" },
+      labels: { overflow: "justify", step: 4 },
+    },
+    legend: {
+      layout: "vertical",
+      align: "right",
+      verticalAlign: "top",
+      x: -40,
+      y: 80,
+      borderWidth: 1,
+    },
+    series: [
+      {
+        name: "Total visits",
+        color: "#fc580c",
+        data: props.endpointData.map((datum) => datum.total_visits),
+      },
+      {
+        name: "Unique visits",
+        color: "#fce2cc",
+        data: props.endpointData.map((datum) => datum.unique_visits),
+      },
+    ],
+  };
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
+};
+
+/**
+ * Props for the [[AdminStats]] component
+ * @interface
+ */
+export interface Props {}
+
+/**
+ * State for the [[AdminStats]] component
+ * @interface
+ */
+interface State {
+  /**
+   * The endpoint statistics data, fetched from the backend
+   * @property
+   */
+  endpointData: EndpointDatum[] | null;
+
+  /**
+   * The date range for the admin stats query, or `null` to query
+   * all existing data
+   * @property
+   */
+  adminDataRange: { begin: moment.Moment; end: moment.Moment } | null;
+
+  /**
+   * The result of the admin stats query, fetched from the backend
+   * @property
+   */
+  adminData: AdminStatsData | null;
+}
+
+/**
+ * The [[AdminStats]] component allows the user to view summary statistics
+ * about the total number of links, users, and visits on Shrunk, as well
+ * as to view statistics about the number of visits to each Flask endpoint
+ * @class
+ */
+export class AdminStats extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      endpointData: null,
+      adminDataRange: null,
+      adminData: null,
+    };
+  }
+
+  async componentDidMount(): Promise<void> {
+    await Promise.all([this.updateAdminData(), this.updateEndpointData()]);
+  }
+
+  /**
+   * Execute a query to the admin stats API endpoint, with time-range parameters
+   * taken from `state.adminDataRange`, if that value is not `null`
+   * @method
+   */
+  updateAdminData = async (): Promise<void> => {
+    const req: Record<string, any> = {};
+    if (this.state.adminDataRange !== null) {
+      req.range = {
+        begin: this.state.adminDataRange.begin.format(),
+        end: this.state.adminDataRange.end.format(),
+      };
+    }
+
+    await fetch("/api/v1/admin/stats/overview", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req),
+    })
+      .then((resp) => resp.json())
+      .then((json) => this.setState({ adminData: json as AdminStatsData }));
+  };
+
+  /**
+   * Fetch the endpoint stats data from the backend
+   * @method
+   */
+  updateEndpointData = async (): Promise<void> => {
+    await fetch("/api/v1/admin/stats/endpoint")
+      .then((resp) => resp.json())
+      .then((json) =>
+        this.setState({ endpointData: json.stats as EndpointDatum[] })
+      );
+  };
+
+  /**
+   * Update the date range for the admin stats query when the admin stats form is submitted
+   * @method
+   * @param values The values from the time range form
+   */
+  submitRangeForm = async (values: {
+    range: moment.Moment[] | null | undefined;
+  }): Promise<void> => {
+    const range = values.range;
+    const adminDataRange =
+      range === undefined || range === null
+        ? null
+        : {
+            begin: range[0],
+            end: range[1],
+          };
+
+    this.setState({ adminDataRange, adminData: null });
+    await this.updateAdminData();
+  };
+
+  render(): React.ReactNode {
+    return (
+      <>
+        <Row className="primary-row">
+          <Col span={24}>
+            <span className="page-title">Admin Statistics</span>
+          </Col>
+        </Row>
+
+        <Row className="primary-row">
+          <Col span={24}>
+            <Row style={{ marginBottom: "12px" }}>
+              <Col span={24}>
+                <Form layout="inline" onFinish={this.submitRangeForm}>
+                  <Form.Item name="range">
+                    <RangePicker />
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Button htmlType="submit" icon={<ArrowRightOutlined />} />
+                  </Form.Item>
+                </Form>
+              </Col>
+            </Row>
+            <Row>
+              {this.state.adminData === null ? (
+                <Spin size="small" />
+              ) : (
+                <>
+                  <span className="info">
+                    Links: {this.state.adminData.links}
+                  </span>
+                  <span className="info">
+                    Visits: {this.state.adminData.visits}
+                  </span>
+                  <span className="info">
+                    Users: {this.state.adminData.users}
+                  </span>
+                </>
+              )}
+            </Row>
+          </Col>
+        </Row>
+
+        <Row className="primary-row">
+          <Col span={24}>
+            <EndpointChart endpointData={this.state.endpointData} />
+          </Col>
+        </Row>
+      </>
+    );
+  }
+}
