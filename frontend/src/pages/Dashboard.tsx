a49be55d803b8e67098bdfe239e1fa51a4cdde07
Grace Chen <gc650@scarletmail.rutgers.edu>
2021-08-19 19:32:56 -0400

    stats page: changed tab type and font title to match the chart titles

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -879,12 +879,12 @@ export class Dashboard extends React.Component<Props, State> {
   render(): React.ReactNode {
     return (
       <>
-        <Row className="primary-row">
+        <Row style={{marginBottom:'20px'}}>
           <Col>
-            <span className="page-title">URL Dashboard</span>
+            <span style={{fontSize:'30pt', color: 'black'}}>URL Dashboard</span>
           </Col>
         </Row>
-        <Row className="primary-row" gutter={[8, 24]}>
+        <Row style={{backgroundColor: '#CBBEC0', padding: '7px', marginBottom:'20px', borderRadius: '9px'}} gutter={[8, 24]}>
           <Col xs={{ span: 24 }} sm={{ span: 9 }}>
             {this.state.userOrgs === null ? (
               <></>
