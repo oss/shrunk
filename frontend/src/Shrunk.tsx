6f9958e1b13bf4cc1aaccd4d5d3692e85e0debcf
Grace Chen <g.chen@rutgers.edu>
2021-11-09 11:40:30 -0500

    chunky text

--- frontend/src/Shrunk.tsx
+++ frontend/src/Shrunk.tsx
@@ -181,20 +181,6 @@ export class Shrunk extends React.Component<Props, State> {
   };
 
   render(): React.ReactNode {
-    function accessibilityInfo() {
-      Modal.info({
-        title: 'Accessibility',
-        content: (
-          <div>
-            <p>Rutgers is an equal access/equal opportunity institution. 
-            Individuals with disabilities are encouraged to direct suggestions, comments, or complaints concerning any accessibility issues 
-            with Rutgers web sites to <a href="mailto:accessibility@rutgers.edu">accessibility@rutgers.edu</a> or complete the
-             <a href="https://rutgers.ca1.qualtrics.com/jfe/form/SV_57iH6Rfeocz51z0"> Report Accessibility Barrier or Provide Feedback Form</a>.</p>
-          </div>
-        ),
-        onOk() {},
-      });
-    }
     return (
       <HashRouter>
         <Layout>
@@ -393,9 +379,12 @@ export class Shrunk extends React.Component<Props, State> {
             />
           </Layout>
           <Footer style={{ textAlign: 'center', color: '#f0f0f0' }}>
+          <p>Rutgers is an equal access/equal opportunity institution. 
+            Individuals with disabilities are encouraged to direct suggestions, comments, or complaints concerning any accessibility issues 
+            with Rutgers web sites to <a href="mailto:accessibility@rutgers.edu">accessibility@rutgers.edu</a> or complete the
+             <a href="https://rutgers.ca1.qualtrics.com/jfe/form/SV_57iH6Rfeocz51z0"> Report Accessibility Barrier or Provide Feedback Form</a>.</p>
             &copy;{new Date().getFullYear()}&mdash;<a href="https://www.rutgers.edu">Rutgers, The State University
-            of New Jersey</a>&#65372;<a onClick={accessibilityInfo}>Accessibility</a>
-            <br/>Questions? Bugs? Contact us:&nbsp;<a href="mailto:oss@oss.rutgers.edu">oss@oss.rutgers.edu</a>
+            of New Jersey</a>&mdash;Questions? Bugs? Contact us:&nbsp;<a href="mailto:oss@oss.rutgers.edu">oss@oss.rutgers.edu</a>
           </Footer>
         </Layout>
       </HashRouter>
