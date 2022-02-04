9d56250a6b00b9cc088c603446c58ccdf1c626e6
Kevin Monisit <monisitkevin@gmail.com>
2022-02-04 13:45:39 -0500

    Explicilty changed marked version; kept typedoc at the compatible

--- frontend/typedocRunner.ts
+++ frontend/typedocRunner.ts
@@ -8,27 +8,29 @@ const td = require('typedoc');
 const ts = require('typescript');
 
 
-async function main() {
-  const app = new td.Application();
-  // For reading typedoc.json - optional
-  app.options.addReader(new td.TypeDocReader());
-  // For reading tsconfig.json - essential
-  app.options.addReader(new td.TSConfigReader());
+const app = new td.Application();
+// For reading typedoc.json - optional
+app.options.addReader(new td.TypeDocReader());
+// For reading tsconfig.json - essential
+app.options.addReader(new td.TSConfigReader());
 
-  app.bootstrap({
-    // can put other options here too, or in typedoc.json/tsconfig.json
-    options: 'typedoc.json',
-    entryPoints: ['./src'],
-  });
+app.bootstrap({
+  // can put other options here too, or in typedoc.json/tsconfig.json
+  options: 'typedoc.json',
+  entryPoints: ['./src'],
+});
 
+const program = ts.createProgram(
+  app.options.getFileNames(),
+  app.options.getCompilerOptions(),
+);
 
-  // Application.convert checks for compiler errors here.
+// Application.convert checks for compiler errors here.
 
-  const project = app.convert();
+const project = app.converter.convert(
+  app.expandInputFiles(app.options.getValue('entryPoints')),
+  program,
+);
 
-  app.generateDocs(project, './docs');
-  app.generateJson(project, './docs.json');
-
-}
-
-main().catch(console.error);
+app.generateDocs(project, './docs');
+app.generateJson(project, './docs.json');
