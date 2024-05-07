b864b4006890d8328d1fb270b49dd207038dcd91
Andrew Hong <andrew.jwn.hong@gmail.com>
2024-05-07 17:47:17 -0400

    Create starting dashboard for linkhub

--- frontend/src/pages/LinkHubDashboard.tsx
+++ frontend/src/pages/LinkHubDashboard.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import './Dashboard.less';
+import { Button, Col, Row } from 'antd/lib';
+import { SearchBox } from '../components/SearchBox';
+import { OrgsSelect } from '../components/OrgsSelect';
+import { PlusCircleFilled } from '@ant-design/icons/lib/icons';
 
 export default function LinkHubDashboard() {
   return (
-    <div>
-      <p>
-        hi random osser, this is just like a temp frontend because i want to
-        finish the backend first before working on thefrontend yknow??
-      </p>
-    </div>
+    <>
+      <Row className="dashboard-title">
+        <Col>
+          <span className="page-title">LinkHub Dashboard</span>
+        </Col>
+      </Row>
+      <Row className="primary-row" gutter={[8, 24]}>
+        <Col xs={{ span: 24 }} sm={{ span: 9 }}>
+          <SearchBox placeholder='Search LinkHubs...' updateQueryString={() => {}} />
+        </Col>
+        <Col className="shrink-link">
+          <Button type="primary" aria-label="create linkhub">
+            <PlusCircleFilled /> Create LinkHub
+          </Button>
+        </Col>
+      </Row>
+      <div className='dashboard-links'>
+
+      </div>
+    </>
   );
 }
