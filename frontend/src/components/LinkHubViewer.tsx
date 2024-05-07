2e4af580d3ae497043a2533465d764366ef75ee8
Andrew Hong <andrew.jwn.hong@gmail.com>
2024-05-07 11:41:52 -0400

    Dummy visuals for linktree done

--- frontend/src/components/LinkHubViewer.tsx
+++ frontend/src/components/LinkHubViewer.tsx
@@ -1,31 +1,73 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import './LinkHubViewer.less';
 
 interface Props {
-    // alias?: string;
+  // alias?: string;
+}
+
+interface DisplayLink {
+  title: string;
+  url: string;
 }
 
 export default function LinkHubViewer(props: Props) {
-    const [title, setTitle] = useState('');
-    const [links, setLinks] = useState([]);
-    const [owners, setOwners] = useState([]);
-  
-    let { alias } = useParams<{alias: string}>();
+  const [title, setTitle] = useState('StarCraft II Tournament @ RU');
+  const [links, setLinks] = useState<DisplayLink[]>([
+    {
+      title: 'Graphic Design Application',
+      url: 'https://google.com/',
+    },
+    {
+      title: 'Caster Application',
+      url: 'https://google.com/',
+    },
+    {
+      title: 'Marketing Application',
+      url: 'https://google.com/',
+    },
+  ]);
+  const [owners, setOwners] = useState([]);
 
-    console.log(alias)
+  let { alias } = useParams<{ alias: string }>();
 
-    useEffect(() => {
-        fetch(`/api/v1/linkhub/${alias}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }).then((data) => {
-            console.log(data)
-        })
+  useEffect(() => {
+    fetch(`/api/v1/linkhub/${alias}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((data) => {
+      console.log(data);
     });
+  });
 
-    return <div>
-        If you are reading this, then you have sucessfully have created two entry points.
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', width: '100vw' }}>
+      <div style={{ textAlign: 'center', width: '100%' }}>
+        <p className="rubik-bold" style={{ fontSize: '3em' }}>
+          {title}
+        </p>
+        <div
+          style={{ display: 'flex', justifyContent: 'center', width: '100%' }}
+        >
+          <div style={{ minWidth: '400px', width: '30vw' }}>
+            {links.map((value, _) => {
+              return (
+                <div
+                  style={{
+                    border: 'solid',
+                    borderRadius: '20px',
+                    marginBottom: '10px',
+                  }}
+                >
+                  <p className="rubik-normal">{value.title}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
+  );
 }
