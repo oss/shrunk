fcc8521af8bee96802b8f9f465792fdc002a1706
Andrew Hong <andrew.jwn.hong@gmail.com>
2024-05-28 00:00:16 -0400

    Fix constant fetches

--- frontend/src/pages/LinkHubPage.tsx
+++ frontend/src/pages/LinkHubPage.tsx
@@ -2,29 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import LinkHubComponent, { DisplayLink } from '../components/LinkHubComponent';
 
-interface Props {
-  // alias?: string;
-}
-
-export default function LinkHubPage(props: Props) {
+export default function LinkHubPage() {
   const [title, setTitle] = useState('');
   const [links, setLinks] = useState<DisplayLink[]>([]);
 
   let { alias } = useParams<{ alias: string }>();
 
   useEffect(() => {
-    const data = fetch(`/api/v1/linkhub/${alias}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then((resp) =>
-      resp.json().then((data) => {
-        setTitle(data['title']);
-        setLinks(data['links']);
-      }),
-    );
-  });
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/api/v1/linkhub/${alias}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await response.json();
+        setTitle(data.title);
+        setLinks(data.links);
+      } catch (error) {}
+    };
+
+    fetchData();
+  }, [alias]);
 
   return <LinkHubComponent title={title} links={links} />;
 }
