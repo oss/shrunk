5221b6af275dc49aede07b97b94d5d1581e4b47a
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2025-02-06 23:38:52 -0500

    Migrate to tabs instead of menu

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -4,27 +4,21 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import { Layout, Spin, Badge, Typography, Menu } from 'antd/lib';
-import Icon, {
+import { Badge, Typography, Tabs, Space, ConfigProvider } from 'antd';
+import {
   LineChartOutlined,
   UserOutlined,
   SafetyOutlined,
   TeamOutlined,
 } from '@ant-design/icons';
 import { Content } from 'antd/lib/layout/layout';
-import Sider from 'antd/lib/layout/Sider';
-import { MenuProps } from 'antd';
+import type { TabsProps } from 'antd';
 import AdminStats from '../components/admin/AdminStats';
 import UserLookup from '../components/admin/UserLookup';
 import UsersProvider from '../contexts/Users';
 import ManageUserAccess from '../components/admin/ManageUserAccess';
 import BlockedLinks from '../components/admin/BlockedLinks';
-
-/**
- * Props for the [[Admin]] component
- * @interface
- */
-export interface Props {}
+import { red } from '@ant-design/colors';
 
 /**
  * Summary information for one role
@@ -34,40 +28,12 @@ interface RoleInfo {
   name: string;
   display_name: string;
 }
-
-const SidebarTabs: MenuProps['items'] = [
-  {
-    key: 'analytics',
-    icon: React.createElement(LineChartOutlined),
-    label: 'Analytics',
-    onClick: () => {
-
-    }
-  },
-  {
-    key: 'users',
-    icon: React.createElement(UserOutlined),
-    label: 'Users',
-    children: [
-      {
-        key: 'user-lookup',
-        label: 'User Lookup',
-      },
-      {
-        key: 'manage-access',
-        label: 'Manage Access',
-      },
-    ],
-  },
-  {
-    key: 'links',
-    icon: React.createElement(SafetyOutlined),
-    label: 'Links',
-  },
-];
-
+/**
+ * Props for the [[Admin]] component
+ * @interface
+ */
 interface AdminProps {
-  userNetid: string
+  userNetid: string;
   userPrivileges: Set<string>;
 }
 
@@ -76,7 +42,7 @@ export default function Admin(props: AdminProps): React.ReactElement {
   const [linksToBeVerified, setLinksToBeVerified] = useState(-1);
   const [powerUserRequestsCount, setPowerUserRequestsCount] = useState(-1);
   const [isDomainEnabled, setIsDomainEnabled] = useState(false);
-  const [selectedView, setSelectedView] = useState("analytics");
+
   const updatePendingPowerUserRequestsCount = async () => {
     const response = await fetch('/api/v1/role_request/power_user/count');
     const json = await response.json();
@@ -91,17 +57,14 @@ export default function Admin(props: AdminProps): React.ReactElement {
 
   useEffect(() => {
     const fetchData = async () => {
-      // Fetch pending links count
       const linksResp = await fetch('/api/v1/security/pending_links/count');
       const linksJson = await linksResp.json();
       setLinksToBeVerified(linksJson.pending_links_count);
 
-      // Fetch roles
       const rolesResp = await fetch('/api/v1/role');
       const rolesJson = await rolesResp.json();
       setRoles(rolesJson.roles);
 
-      // Fetch power user requests
       await updatePendingPowerUserRequestsCount();
       await updateIsDomainEnabled();
     };
@@ -109,116 +72,86 @@ export default function Admin(props: AdminProps): React.ReactElement {
     fetchData();
   }, []);
 
-  const adminCards = [
+  const items: TabsProps['items'] = [
     {
-      title: 'Admin Statistics',
-      icon: <LineChartOutlined />,
-      link: '/admin/stats',
+      key: 'analytics',
+      label: (
+        <Space direction="horizontal">
+          <LineChartOutlined />
+          Analytics
+        </Space>
+      ),
+      children: <AdminStats />,
     },
     {
-      title: 'User Lookup',
-      icon: <UserOutlined />,
-      link: '/admin/user_lookup',
+      key: 'user-lookup',
+      label: (
+        <Space direction="horizontal">
+          <UserOutlined />
+          User Lookup
+        </Space>
+      ),
+      children: (
+        <UsersProvider>
+          <UserLookup />
+        </UsersProvider>
+      ),
     },
     {
-      title: 'Unsafe Links Pending Verification',
-      icon: <SafetyOutlined />,
-      link: '/admin/link_security',
-      badge:
-        linksToBeVerified === -1 ? (
-          <Spin size="small" />
-        ) : (
-          <Badge count={linksToBeVerified} />
-        ),
+      key: 'manage-access',
+      label: (
+        <Space direction="horizontal">
+          <TeamOutlined />
+          Manage Access
+        </Space>
+      ),
+      children: (
+        <UsersProvider>
+          <ManageUserAccess
+            userNetid={props.userNetid}
+            userPrivileges={props.userPrivileges}
+          />
+        </UsersProvider>
+      ),
     },
     {
-      title: 'Pending Power User Role Requests',
-      icon: <TeamOutlined />,
-      link: '/admin/role_requests/power_user',
-      badge:
-        powerUserRequestsCount === -1 ? (
-          <Spin size="small" />
-        ) : (
-          <Badge count={powerUserRequestsCount} />
-        ),
+      key: 'links',
+      label: (
+        <Space direction="horizontal">
+          <SafetyOutlined />
+          Links{' '}
+          {linksToBeVerified > 0 && (
+            <Badge count={linksToBeVerified} style={{ marginLeft: '8px' }} />
+          )}
+        </Space>
+      ),
+      children: (
+        <UsersProvider>
+          <BlockedLinks name="power_user" />
+        </UsersProvider>
+      ),
     },
   ];
 
-  if (isDomainEnabled) {
-    adminCards.push({
-      title: 'Custom Domains',
-      icon: <Icon type="link" />,
-      link: '/admin/domain',
-    });
-  }
   return (
     <>
       <Typography.Title>Administrator Controls</Typography.Title>
-      <Layout>
-        <Sider 
-          width={200} 
+      <Content>
+        <ConfigProvider
+          theme={{
+            inherit: true,
+            components: {
+              Tabs: {
+                itemHoverColor: red[6],
+                itemSelectedColor: red[6],
+                itemActiveColor: red[6],
+              },
+            },
+          }}
         >
-          <div style={{ display: 'flex', height: '100%' }}>
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={['analytics']}
-              style={{ 
-                height: '100%',
-                paddingRight: '12px', 
-                borderRight: '2px solid #d9d9d9',
-              }}
-              items={SidebarTabs}
-              onClick={(e) => {
-                switch(e.key) {
-                  case 'analytics':
-                    setSelectedView('analytics');
-                    break;
-                  case 'user-lookup':
-                    setSelectedView('user-lookup');
-                    break;
-                  case 'manage-access':
-                    setSelectedView('manage-access');
-                    break;
-                  case 'links':
-                    setSelectedView('links');
-                    break;
-                  default:
-                    break;
-                }
-              }}
-            />
-          </div>
-        </Sider>
-        <Content style={{ padding: '0 24px', minHeight: 280 }}>
-            {
-              selectedView === 'analytics' && ( <AdminStats /> )
-            }
-
-            {
-              selectedView === 'user-lookup' && ( 
-              <UsersProvider>
-                <UserLookup />
-              </UsersProvider>
-            )
-            }
-
-            {
-              selectedView === 'manage-access' && ( 
-                <UsersProvider>
-                  <ManageUserAccess userNetid={props.userNetid} userPrivileges={props.userPrivileges} />
-                </UsersProvider>
-              )
-            }
-
-            {
-              selectedView === 'links' && ( 
-                <UsersProvider>
-                  <BlockedLinks name={"power_user"} /> 
-                </UsersProvider>
-              )
-            }
-        </Content>
-      </Layout>
+          <Tabs defaultActiveKey="analytics" items={items} type="card" />
+        </ConfigProvider>
+      </Content>
     </>
   );
 }
