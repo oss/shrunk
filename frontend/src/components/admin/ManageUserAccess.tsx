9b17e6f79293e289a8fadd963f187bb2c0856896
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2025-02-07 00:04:41 -0500

    Remove remaining instance of Row

--- frontend/src/components/admin/ManageUserAccess.tsx
+++ frontend/src/components/admin/ManageUserAccess.tsx
@@ -3,39 +3,30 @@
  * @packageDocumentation
  */
 
-import { Button, Col, Row, Table, Tag, Spin } from 'antd/lib';
 import React from 'react';
-import {
-  Operation,
-  useUsers,
-} from '../../contexts/Users';
-import { ConfigProvider, Typography } from 'antd';
-import LookupTableHeader from './LookupTableHeader';
-import { lightTheme } from '../../theme';
+import { Typography } from 'antd';
 import { PendingRoleRequests } from './PendingRoleRequests';
 
-
 interface ManageUserAccessProps {
-    userNetid: string;
-    userPrivileges: Set<string>;
+  userNetid: string;
+  userPrivileges: Set<string>;
 }
 
 /**
- * The [[ManageUserAccess]] component displays a table of pending power user requests. Admins can manage 
+ * The [[ManageUserAccess]] component displays a table of pending power user requests. Admins can manage
  * and approve/deny these requests through this component.
  * @returns the [[ManageUserAccess]] component
  */
 const ManageUserAccess: React.FC<ManageUserAccessProps> = (props) => {
   return (
     <>
-      <Row className="secondary-row" style={{ marginBottom: 0 }}>
-        <Col>
-            <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem', justifyContent: 'space-between', alignItems: 'baseline' }}>
-              <Typography.Title level={3} style={{ marginTop: 0, marginBottom: 16 }}>Pending Requests</Typography.Title>
-            </div>
-        </Col>
-      </Row>
-      <PendingRoleRequests name={"power_user"} userPrivileges={props.userPrivileges} />
+      <Typography.Title level={3} style={{ marginTop: 0, marginBottom: 16 }}>
+        Pending Requests
+      </Typography.Title>
+      <PendingRoleRequests
+        name={'power_user'}
+        userPrivileges={props.userPrivileges}
+      />
     </>
   );
 };
