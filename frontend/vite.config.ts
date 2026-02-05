1ec455155edad50b1274aa1ebc69114544876379
wispl <wispl.8qbkk@slmail.me>
2026-02-04 19:34:42 -0500

    feat: use pvc for logs and inject env variables
    fixup: frontend dockerfile changes
    
    fixup: handle vite change
    
    fixup: small tweaks
    
    fixup: small tweaks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -28,6 +28,16 @@ export default defineConfig({
     hmr: {
       clientPort: 4343,
     },
+    // DEV: Proxy /api and urls not starting with /app/ to backend,
+    // PROD: httpd is used
+    proxy: {
+      '/api': {
+        target: 'http://backend:3050'
+      },
+      '^(?!\/app\/).*': {
+	target: 'http://backend:3050'
+      }
+    }
   },
   build: {
     outDir: 'dist',
