b28cf5ade68e992c6b4847725519a5da8f0cbf43
wispl <wispl.8qbkk@slmail.me>
2026-04-17 04:30:38 -0400

    fix: tweaks to shibboleth dockerfile and kustomization.yml

--- shib-sp/entrypoint.sh
+++ shib-sp/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
+# This is mainly for use in k8, since mounting a volume destroys all existing
+# files inside the directory, we need to instead mount to some intermediate
+# location and then copy over the config files, in order to retain the config.
 cp /etc/shibconfig/* /etc/shibboleth
 exec /usr/sbin/shibd -f -F -c /etc/shibboleth/shibboleth2.xml -w 30
