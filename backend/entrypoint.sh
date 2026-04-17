3d0ef03763b053993b8a7f68d8eb94e352e24880
wispl <wispl.8qbkk@slmail.me>
2026-04-17 04:28:36 -0400

    feat: swap to uswgi, use uswgi protocol
    Use uswgi binary protocol in Apache. This plays better with server variables
    which Apache and Shib uses to inject attributes into requests which are passed
    to the backend. Swap from gunicorn to uswgi for now as no versions of gunicorn
    on python 3.6 supports the uswgi protocol

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-# TODO: not sure if we need this
-mkdir logs
-exec gunicorn --bind 0.0.0.0:3050 --worker-tmp-dir  /dev/shm  --workers=2 --threads=4 --worker-class=gthread "${FLASK_APP}:create_app()"
+# exec gunicorn --bind 0.0.0.0:3050 --worker-tmp-dir /dev/shm  --workers=2 --threads=4 --worker-class=gthread --worker-class asgi --protocol uwsgi --uwsgi-allow-from '*' "${FLASK_APP}:create_app()" 
+exec uwsgi --module "${FLASK_APP}:create_app()" --master --processes 4 --threads 2 --socket 0.0.0.0:3050
